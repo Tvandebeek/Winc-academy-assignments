e565c6a14efafbe8bd200d4ebf5c7ff91b82fcdb
Tvandebeek <90839760+Tvandebeek@users.noreply.github.com>
2021-12-14 22:48:39 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log("Hello Winc Academy students")
+
+//this is a large sum
+/*
+let som = 1230941*1823794;
+console.log (som);
+let som2 = 637263/54;
+console.log (som2);
+*/
+
+let som = 1230941*1823794;
+console.log (som);
+let som2 = 637263/54;
+console.log (som2);

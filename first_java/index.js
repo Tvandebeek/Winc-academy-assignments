54ef4e156bf42c15de8b26d2a21820812786c2e1
Tvandebeek <90839760+Tvandebeek@users.noreply.github.com>
2021-12-14 21:31:37 +0100

    Add files via upload

---
+++ first_java/index.js
@@ -0,0 +1,25 @@
+//this is my first js code
+console.log ('Hello Winc Academy');
+
+let name = 'Teun';
+console.log(name);
+
+let som = 4+4;
+console.log (som);
+
+let sub = 8-4;
+let mult = 2*4;
+let div= 8/2;
+let y = 5;
+let x = y%2;
+console.log(sub);
+console.log(mult);
+console.log (div);
+console.log (y);
+console.log (x);
+
+let age = 42;
+console.log(age);
+
+let leeftijd = 'fortytwo';
+console.log (leeftijd);

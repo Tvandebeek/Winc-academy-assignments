a6c774ebedde813229d9425da95f12f4c9121e67
Tvandebeek <90839760+Tvandebeek@users.noreply.github.com>
2021-12-16 23:03:22 +0100

    Add files via upload

--- pub.js
+++ pub.js
@@ -1,5 +1,10 @@
 let age=20;
 
+var isFemale, isMale;
+isFemale = false;
+
+const firstName = "Sarah"
+
 if (age>=18){
     console.log ("you are allowed inside, welcome!");
 }
@@ -7,10 +12,6 @@ if (age>=18){
 else{
     console.log ("please don't come in, you are too young.");
 }
-
-var isFemale, isMale;
-
-isFemale = false;
 
 if (isFemale==true){
     console.log ("You can proceed to check-out");
@@ -29,3 +30,58 @@ let driverstatus = ("bob")
 else{
     console.log ("you are not fit to drive, please call a cab");
 }
+
+if (age > 18 && age < 25) { 
+  console.log("You get 50% off!");
+}
+
+else {
+  console.log ("regular costumer");
+}
+
+if (firstName = "Abraham" || "Sarah") {
+  console.log ("Hurray, you've earned a free beer");
+}
+
+else {
+  console.log ("Welcome at or 50th birthday party!");
+}
+
+
+
+/*oplossing winc
+
+// Verander deze input om te zien of je code werkt.
+// Comment bijvoorbeeld regel 8 tot 11 weer aan, en regel 3 tot 5 uit.
+const age = 18;
+const isFemale = true;
+const driverStatus = "bob";
+
+/*
+Testdata
+const age = 16; 
+const isFemale = false;
+const driverStatus = 808; 
+*/
+
+/*if (age >= 18) {
+  console.log("Je mag naar binnen");
+} else {
+  console.log("Nope, je mag niet naar binnen");
+}
+
+if (isFemale) {
+  console.log("Je ticket zegt dat je een vrouw bent!");
+} else {
+  console.log(
+    "Helaas je hebt aangegeven op je ticket dat je een man bent, je mag niet naar binnen"
+  );
+}
+
+if (driverStatus === "bob") {
+  console.log("Je bent de bob, je mag autorijden");
+} else {
+  console.log("Je bent geen bob, hier met die autosleutels");
+}*/
+
+

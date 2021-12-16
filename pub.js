1834bbd9630fd2010ee06994ab02dbfe14051eca
Tvandebeek <90839760+Tvandebeek@users.noreply.github.com>
2021-12-16 22:22:05 +0100

    Add files via upload

---
+++ pub.js
@@ -0,0 +1,31 @@
+let age=20;
+
+if (age>=18){
+    console.log ("you are allowed inside, welcome!");
+}
+
+else{
+    console.log ("please don't come in, you are too young.");
+}
+
+var isFemale, isMale;
+
+isFemale = false;
+
+if (isFemale==true){
+    console.log ("You can proceed to check-out");
+}
+
+else{
+    console.log ("this is a ladies-only event");
+}
+
+let driverstatus = ("bob")
+
+    if (driverstatus == "bob"){
+        console.log ("Drive safely!");
+}
+
+else{
+    console.log ("you are not fit to drive, please call a cab");
+}

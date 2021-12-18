d1596df482d16b5cb0d7e742042408191b973ad1
Tvandebeek <90839760+Tvandebeek@users.noreply.github.com>
2021-12-18 21:46:24 +0100

    Add files via upload

---
+++ array.js
@@ -0,0 +1,16 @@
+let kleuren =['groen','blauw','rood'];
+let groet = ({greeting: "hi, I am an object"})
+console.log(kleuren);
+
+console.log(kleuren[2]);
+
+kleuren.push('yellow');
+console.log(kleuren);
+
+kleuren.push(5);
+console.log(kleuren);
+
+kleuren.push(groet);
+console.log(kleuren);
+
+console.log(kleuren[5]);

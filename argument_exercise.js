a5dc0a493033afe8da63f3ff6cf1ca04e9bfb481
Tvandebeek <90839760+Tvandebeek@users.noreply.github.com>
2021-12-19 13:49:13 +0100

    Add files via upload

---
+++ argument_exercise.js
@@ -0,0 +1,17 @@
+
+const paint = function(color, richting){
+    console.log("The " + richting + 'wall has been painted ' + color);
+}
+    const color1= "red";
+    const color2= "green";
+    const color3='grey';
+    const color4= 'orange';
+
+
+    const richting1= 'north';
+    const richting2= 'south';
+    const richting3= 'east';
+    const richting4= 'west'; 
+
+
+paint(color3, richting3);

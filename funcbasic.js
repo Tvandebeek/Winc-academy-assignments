e0b1897dd5731a2cbc84737ff0f332137615e9b1
Tvandebeek <90839760+Tvandebeek@users.noreply.github.com>
2021-12-19 12:42:47 +0100

    Add files via upload

---
+++ funcbasic.js
@@ -0,0 +1,25 @@
+const makePizzaDough = function () {
+    console.log("Place the sugar, salt, olive oil, water, 1 cup of flour, yeast, and remaining cup of flour into a standing mixer's work bowl");
+    console.log("Knead for 15 minutes");
+    console.log("Roll the pizza dough into a smooth ball");
+    console.log("Cover with plastic wrap and refrigerate for 18 to 24 hours");
+    console.log("Go and make pizza");
+}; 
+
+const makeSushi = function () {
+    console.log("Prep the rice & Nori. Lay out the nori on the bamboo mat");
+    console.log("Spread the rice on the Nori");
+    console.log("Add fillings to the bottom quarter of the Nori");
+    console.log("Roll the Sushi");
+    console.log("Move to cutting board, cut and serve");
+}; 
+
+makeSushi(); 
+
+makeSushi();
+
+makeSushi();
+
+makePizzaDough();
+
+makePizzaDough();

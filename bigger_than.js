5210410a686eddac8e9dabc62ab823e335953a89
Tvandebeek <90839760+Tvandebeek@users.noreply.github.com>
2021-12-20 23:09:08 +0100

    Add files via upload

---
+++ bigger_than.js
@@ -0,0 +1,14 @@
+
+const nummer = 105;
+
+const biggerThan100 = function(nummer){
+   
+    if (nummer>100){
+        outcome = ("true");
+    } else {
+       outcome =("false");
+    }
+    return(outcome);
+    }
+
+    console.log (outcome)

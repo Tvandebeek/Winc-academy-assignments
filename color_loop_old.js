2aa1e6942234619042f60cad1abf30d7d560f05d
Tvandebeek <90839760+Tvandebeek@users.noreply.github.com>
2021-12-26 21:05:16 +0100

    Add files via upload

---
+++ color_loop_old.js
@@ -0,0 +1,42 @@
+//WHILE LOOP
+// let colors =['yellow','blue','red','orange'];
+
+// let i = 0;
+// let colorsLength = colors.length;
+// while(i < colorsLength ) {
+//    let color = colors[i];
+//    i++;
+
+// console.log(color);
+// }
+
+//FOR-LOOP
+
+// let colors2 =['green','purple','mauve','pink'];
+
+// let colors2Length = colors2.length;
+// for(let i = 0 ; i < colors2; i++) {
+//      let color2 = colors2[i];
+
+// console.log(color2);
+// }
+
+// //FOR EACH
+
+const color3 = ['rood', 'wit', 'blauw', 'geel'];
+
+color3.forEach(element => console.log(element));
+
+
+// My while-loop takes 8 lines
+// My for-loop takes 6 lines
+// My for-each method takes 2 lines
+
+//advantage, easier to read, more understandable and no i-factors etc. And it doesn't need 
+//to be told to stop.
+
+//If a property is modified in one iteration and then visited at a later time, its value in 
+//the loop is its value at that later time. A property that is deleted before it has been visited 
+//will not be visited later. Properties added to the object over which iteration is occurring may 
+//either be visited or omitted from iteration.
+
